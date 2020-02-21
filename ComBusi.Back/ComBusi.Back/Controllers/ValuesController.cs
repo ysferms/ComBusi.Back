f5da92cdd0809e92f2174a4964a6e3bedae7733a
BerkAcikgoz <berk.acikgoz1@gmail.com>
2020-02-21 22:38:17 +0300

    test2

--- ComBusi.Back/ComBusi.Back/Controllers/ValuesController.cs
+++ ComBusi.Back/ComBusi.Back/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace ComBusi.Back.Controllers
     [HttpGet]
     public ActionResult<IEnumerable<string>> Get()
     {
-      return new string[] { "value1", "value2" };
+      return new string[] { "test", "value2" };
     }
 
     // GET api/values/5
